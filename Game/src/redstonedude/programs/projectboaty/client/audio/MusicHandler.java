7e4582bbb96c239fa49fb12a8e4016f1460437f6
D-u-s-k <ramenrampage@tuta.io>
2018-02-12 15:07:20 +0000

    Do what you want cuz a pirate is free

--- Game/src/redstonedude/programs/projectboaty/client/audio/MusicHandler.java
+++ Game/src/redstonedude/programs/projectboaty/client/audio/MusicHandler.java
@@ -1,17 +1,19 @@
 package redstonedude.programs.projectboaty.client.audio;
 
 import java.io.File;
+
+import javafx.embed.swing.JFXPanel;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
 public class MusicHandler {
-	
-	public static void musicTester() {
-	String bip = "resources/audio/music/bip.mp3";
-	Media hit = new Media(new File(bip).toURI().toString());
-	MediaPlayer mediaPlayer = new MediaPlayer(hit);
-	mediaPlayer.play();
-	
-	}
 
+	public static void musicTester() {
+		final JFXPanel fxp = new JFXPanel();
+		String bip = System.getProperty("user.dir") + "/resources/audio/music/bip.mp3";
+		Media hit = new Media(new File(bip).toURI().toString());
+		MediaPlayer mediaPlayer = new MediaPlayer(hit);
+		mediaPlayer.play();
+
+	}
 }
